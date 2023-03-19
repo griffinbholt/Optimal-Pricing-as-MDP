ba5703683a1117743f19da26294fcd3b8ee1fc16
Allison <allisonkguman@gmail.com>
2023-03-18 19:42:47 -0700

    initial commit for V1 experiments

---
+++ V1/experiments.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def main
+
+def get_action(state, mdp, epsilon):
+    # TODO add other action strategies
+    get_epsilon_greedy_action(state, mdp, epsilon)
+
+def get_epsilon_greedy_action(state, mdp, epsilon):
+    # note: plan on using epsilon decay for V1 problem desc
+
+
+if __name__ == "__main__":
+    main()
